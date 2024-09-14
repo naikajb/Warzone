c62517dec7844e8d03f41d4406a24c1a8beeebd3
Naika JB <107156607+naikajb@users.noreply.github.com>
2024-09-14 17:45:05 -0400

    base files for project

---
+++ Player.cpp
@@ -0,0 +1,9 @@
+/**
+ * PLAYER CLASS FEATURES:
+ * 1. Player owns collection of Territory objects
+ * 2. Player owns a hand of Card objects
+ * 3. Player contains methods toDefend(), toAttack()
+ * 4. Player contains issueOrder() method to creates and Order object and adds it to the list
+ * 5. Driver names testPlayers() method to test the Player class
+ */
+
