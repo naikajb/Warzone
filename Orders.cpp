f9c262b690c534c846cd1335320170cb99f2b305
Tanya So Tin Yan <tanya.sty99@gmail.com>
2024-09-16 01:41:45 -0400

    Implement Derived Classes (Deploy, Advance, Bomb)

--- Orders.cpp
+++ Orders.cpp
@@ -14,3 +14,58 @@ std::ostream& operator<<(std::ostream& out, const Order& order){
     }
     return out;
 }
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+DeployOrder::DeployOrder(){
+    OrderDescription = "Deploy Order";
+}
+
+DeployOrder::~DeployOrder() {}
+
+bool DeployOrder::validateOrder() const{
+    return true;
+}
+
+void DeployOrder::executeOrder(){
+    if(validateOrder()){
+        isExecuted = true;
+        OrderEffect = "Armies are deployed";
+    }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+AdvanceOrder::AdvanceOrder(){
+    OrderDescription = "Advance Order";
+}
+
+AdvanceOrder::~AdvanceOrder() {}
+
+bool AdvanceOrder::validateOrder() const{
+    return true;
+}
+
+void AdvanceOrder::executeOrder(){
+    if(validateOrder()){
+        isExecuted = true;
+        OrderEffect = "Armies are advanced";
+    }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+BombOrder::BombOrder(){
+    OrderDescription = "Bomb Order";
+}
+
+BombOrder::~BombOrder() {}
+
+bool BombOrder::validateOrder() const{
+    return true;
+}
+
+void BombOrder::executeOrder(){
+    if(validateOrder()){
+        isExecuted = true;
+        OrderEffect = "Territory is bombed";
+    }
+}
+
+
