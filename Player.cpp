250e6f6367461e6ed2a4aec62f76269234c7182b
Naika JB <107156607+naikajb@users.noreply.github.com>
2024-09-17 17:25:16 -0400

    added most methods to header file + start of implementation

--- Player.cpp
+++ Player.cpp
@@ -7,3 +7,45 @@
  * 5. Driver names testPlayers() method to test the Player class
  */
 
+#include "Player.h"
+using namespace std;
+
+Player::Player() {
+    orders = new OrderList(); 
+}
+
+Player :: Player(const Player& orig){
+
+    //copy every territory from the original player to the new player
+    for (Territory* territories: orig.territories){
+        this->territories.push_back(territories);
+    }
+
+    //copy every card from the original player to the new player
+    for (Cards* card: orig.hand){
+        this->hand.push_back(card);
+    }
+
+    //copy every order from the original player to the new player
+    orders = new OrderList(*orig.orders);
+}
+
+// Player destructor
+Player::~Player() {
+    // for (Order* order: orders->getOrders()){
+    //     delete order;
+    // }
+    delete orders; //TODO: check if i need to delete the orders in the list first
+
+    for (Territory* territory: territories){
+        delete territory;
+    }
+    //TODO: check if i need to delete the cards in the hand first
+}
+
+// adds order to the player's list of orders
+void Player::issueOrder(Order* order){
+    orders->addOrder(order);
+}
+
+// adds territory to the player's list of territories
---
+++ Player.h
@@ -0,0 +1,40 @@
+#include <iostream> 
+#include <vector> 
+#include "Territory.h"
+#include "Orders.h"
+#include "Cards.h"
+
+
+class Player{
+
+    private: 
+
+        std::vector<Territory*> territories; // collection of Territory objects
+        std::vector<Cards*> hand; // hand of Card objects
+        OrderList* orders; // list of Order objects
+
+    public: 
+        Player();
+        Player(const Player& orig);
+        ~Player(); //destructor function 
+    
+        // Player owns collection of Territory objects
+        void addTerritory(Territory* territory);
+        void removeTerritory(Territory* territory);
+
+        std::vector<Territory*> toDefend();  //returns territories to be defended 
+        std::vector<Territory*> toAttack();  //returns territories to be attacked
+        
+        void issueOrder(Order* order);  //adds order to player's list of orders
+        
+        void addCard(Cards* card);  //adds card to player's hand
+        void removeCard(Cards* card);  //removes card from player's hand
+        
+        void printPlayer();  //prints player's information
+        void printTerritories();  //prints player's territories
+        void printOrders();  //prints player's orders
+
+        // Driver names testPlayers() method to test the Player class
+        void testPlayers();
+        
+};
