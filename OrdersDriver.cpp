d513035ddf0502b2720155213535f1d8ce9354f2
Tanya So Tin Yan <tanya.sty99@gmail.com>
2024-09-24 16:41:28 -0400

    Added comments to demonstrate OrdersList's implementation

--- OrdersDriver.cpp
+++ OrdersDriver.cpp
@@ -1,10 +1,13 @@
 #include "Orders.h"
 #include <iostream>
 
+// Function to test the functuonality of the OrdersList class
 void testOrdersLists() {
+
+    // Create an instance of OrdersList to manage orders
     OrdersList ordersList;
 
-    // Create different orders
+    // Create different TYPES of orders 
     Order* deploy = new Deploy();
     Order* advance = new Advance();
     Order* bomb = new Bomb();
@@ -12,7 +15,7 @@ void testOrdersLists() {
     Order* airlift = new Airlift();
     Order* negotiate = new Negotiate();
 
-    // Add orders to the list
+    // Add created orders to the orders list
     ordersList.addOrder(deploy);
     ordersList.addOrder(advance);
     ordersList.addOrder(bomb);
@@ -20,29 +23,31 @@ void testOrdersLists() {
     ordersList.addOrder(airlift);
     ordersList.addOrder(negotiate);
 
-    // Print initial list
+    // Print the initial list of orders
     std::cout << "Initial Orders List:" << std::endl;
     std::cout << ordersList << std::endl;
 
-    // Move orders
+    // Move the 1rst order (deploy) to the end of the list
     ordersList.move(0, 5);  // Move the first order to the end
     std::cout << "After Moving Deploy to Last:" << std::endl;
     std::cout << ordersList << std::endl;
 
-    // Remove an order
+    // Remove an order from the orders list
     ordersList.remove(2);  // Remove Bomb order
     std::cout << "After Removing Bomb Order:" << std::endl;
     std::cout << ordersList << std::endl;
 
-    // Execute some orders
+    // Execute the 'deploy' and the 'advance' orders to change their state
     deploy->executeOrder();
     advance->executeOrder();
 
+    // Print the orders list after executing 'deploy' order and 'advance' order
     std::cout << "After Executing Deploy and Advance Orders:" << std::endl;
     std::cout << ordersList << std::endl;
 }
 
+// Main function to run the test
 int main() {
-    testOrdersLists();
+    testOrdersLists(); //Call the function to test OrdersList functionality
     return 0;
 }
