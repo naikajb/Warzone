83b6f31f9f4281670cecd912f0129c13c33e89f2
Jacob <jakelid111@gmail.com>
2024-10-02 18:49:19 -0400

    final comments added // testLoadMaps() added to MainDriver

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -2,8 +2,15 @@
 
 // Include headers for each part's test function
 #include "OrdersDriver.h"
+#include "MapDriver.h"
 
 int main(){
+
+    std::cout << "Testing Part 1 - Map Component :" << std::endl;
+
+    // Call funcion to test the Map(s)
+    testLoadMaps();
+
     std::cout << "Testing Part 3 - Orders List Component :" << std::endl;
 
     // Call funcion to test the Orders list
