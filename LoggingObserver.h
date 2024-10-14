1ac49c02611eb569fc4d6801efe1454d6facd2c7
Naika JB <107156607+naikajb@users.noreply.github.com>
2024-10-14 13:47:54 -0400

    Add LoggingObserver/Driver files

---
+++ LoggingObserver.h
@@ -0,0 +1,11 @@
+// 
+#ifndef LOGGINGOBSERVER_H
+#define LOGGINGOBSERVER_H
+
+//saveCommand(), execute(), saveEffect(), addOrder(), transition() use the Notify() method
+class Observer; 
+//classes Order, OrderList, GameEngine, Command and CommandProcessor are Subject and ILoggable objects
+class Subject;
+class ILoggable; 
+
+#endif
