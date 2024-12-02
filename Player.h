69cb16114913486642e0df53b7ea02ea25a01428
shamma13 <shammamarkis@hotmail.com>
2024-12-02 00:24:03 -0500

    imported new version of code from ihana's branch

--- Player.h
+++ Player.h
@@ -1,3 +1,73 @@
+// #ifndef PLAYER_H
+// #define PLAYER_H
+
+// #include <iostream> 
+// #include <string>
+// #include <vector> 
+// #include <algorithm>
+// #include "Orders.h"
+// #include "Cards.h"
+// #include "Map.h"
+// #include "LoggingObserver.h"
+// using std::sort;
+// class OrdersList;
+// class Order;
+
+// class Player{
+
+//     private: 
+//         std::string name; // name of the player
+//         std::vector<Territory*> territories; // collection of Territory objects
+//         Hand* hand; // hand of Card objects
+//         OrdersList* orders; // list of Order objects
+//         bool canDrawCard;
+//         int reinforcementPool; // nb of armies recieved added
+//         int reinforcementTemp;
+//         Observer* observer;
+
+//     public: 
+//         Player(Observer*, const std::string& playerName); //constructor function
+//         Player(const Player& orig);
+//         ~Player(); // TODO: implement destructor function 
+    
+//         // Player owns collection of Territory objects
+//         void addTerritory(Territory* territory);
+//         void removeTerritory(Territory* territory);
+
+//         std::vector<Territory*> getTerritories(); // getter of a list of territories the player has
+//         void setReinforcementPool(int);
+//         int getReinforcementPool();
+//         int getReinforcementTemp();
+
+//         std::vector<Territory*> toDefend();  //returns territories to be defended 
+//         std::vector<Territory*> toAttack();  //returns territories to be attacked
+        
+//         void issueOrder(Order *order);  //adds order to player's list of orders
+        
+//         //stream insertion operator to print player's information
+//         friend std::ostream& operator<<(std::ostream& out, const Player& player);
+        
+//         void addCard(Card* card);  //adds card to player's hand
+//         void removeCard(Card* card);  //removes card from player's hand
+//         Hand *getHand(); // returns players hand
+
+//         OrdersList* getOrderList();
+        
+//         void printTerritories();  //prints player's territories
+//         // void printOrders();  //prints player's orders
+
+//         std::string getPlayerName() const;  //returns player's name
+
+//         void AttachObserver(Observer* observer);  //attaches observer to player
+
+//         void drewCard();
+//         void roundReset();
+//         bool getCanDrawCard();
+        
+// };
+
+// #endif // PLAYER_H
+
 #ifndef PLAYER_H
 #define PLAYER_H
 
@@ -5,18 +75,23 @@
 #include <string>
 #include <vector> 
 #include <algorithm>
+#include <random>
 #include "Orders.h"
 #include "Cards.h"
 #include "Map.h"
 #include "LoggingObserver.h"
+#include "PlayerStrategies.h"
 using std::sort;
 class OrdersList;
 class Order;
+class Territory;
+class PlayerStrategy;
+
 
 class Player{
 
     private: 
-        std::string name; // name of the player
+        std::string playerName; // name of the player
         std::vector<Territory*> territories; // collection of Territory objects
         Hand* hand; // hand of Card objects
         OrdersList* orders; // list of Order objects
@@ -24,9 +99,10 @@ class Player{
         int reinforcementPool; // nb of armies recieved added
         int reinforcementTemp;
         Observer* observer;
+        PlayerStrategy* strategy;
 
     public: 
-        Player(Observer*, const std::string& playerName); //constructor function
+        Player(Observer*,string playerName); //constructor function
         Player(const Player& orig);
         ~Player(); // TODO: implement destructor function 
     
@@ -36,8 +112,15 @@ class Player{
 
         std::vector<Territory*> getTerritories(); // getter of a list of territories the player has
         void setReinforcementPool(int);
+        void setReinforcementTemp(int);
         int getReinforcementPool();
         int getReinforcementTemp();
+        PlayerStrategy* getPlayerStrategy();
+        void setPlayerStrategy(PlayerStrategy*);
+        static void assignStrategies(std::vector<Player *>& players);
+
+        Observer* getObserver();
+
 
         std::vector<Territory*> toDefend();  //returns territories to be defended 
         std::vector<Territory*> toAttack();  //returns territories to be attacked
@@ -46,6 +129,7 @@ class Player{
         
         //stream insertion operator to print player's information
         friend std::ostream& operator<<(std::ostream& out, const Player& player);
+        Player &operator=(const Player&);
         
         void addCard(Card* card);  //adds card to player's hand
         void removeCard(Card* card);  //removes card from player's hand
@@ -56,7 +140,7 @@ class Player{
         void printTerritories();  //prints player's territories
         // void printOrders();  //prints player's orders
 
-        std::string getPlayerName() const;  //returns player's name
+        std::string getPlayerName();  //returns player's name
 
         void AttachObserver(Observer* observer);  //attaches observer to player
 
@@ -65,5 +149,6 @@ class Player{
         bool getCanDrawCard();
         
 };
+
 
 #endif // PLAYER_H
